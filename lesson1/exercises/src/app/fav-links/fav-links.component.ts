c1677118e736cd54cb631e3fa588c035afe74080
Angela Dudley <angela.dudley14@gmail.com>
2020-09-13 16:35:29 -0500

    Exercise 1 completed.

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
+  favLinksTitle = 'These are the links.'
   favLinks = ['https://www.snopes.com', 'https://www.factcheck.org'];
+  img = 'https://media0.giphy.com/media/USV0ym3bVWQJJmNu3N/200w.webp?cid=ecf05e47k1cb5n797ugjbvoqpdqsdfwqvp9t55gsgq4bca03&rid=200w.webp'
   constructor() { }
 
   ngOnInit() {
